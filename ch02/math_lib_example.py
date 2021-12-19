0ee9e73cf502dbd131385e9adf8b5254a3f313c0
Vagacoder <qiruihu@gmail.com>
2021-12-19 23:16:42 +0800

    2021-12-19 update ch02 examples

---
+++ ch02/math_lib_example.py
@@ -0,0 +1,23 @@
+
+# ! import math module (a python standard library)
+
+# * import method 1
+from math import sqrt
+
+result1 = sqrt(36)
+print('square root of 36 is:', result1)
+
+
+# * import method 2
+import math
+
+result2 = math.sqrt(49)
+print('square root of 49 is:', result2)
+
+
+# trunc()
+pi = 3.1415926
+print('truncated of pi is:', math.trunc(pi))
+
+# cos()
+print('cosine of pi is:', math.cos(pi))
---
+++ ch02/syntax2_2.py
@@ -0,0 +1,28 @@
+
+# ! Syntax 2.2
+# * Calling Functions
+
+# abs()
+x = -173
+distance = abs(x)
+print('distance:', distance)
+
+# round()
+price = 1.83
+rate = 0.55
+tax = round(price * rate, 2)
+print('tax is', tax)
+
+# min()
+price1 = 1.67
+price2 = 0.56
+price3 = 2.99
+price4 = 0.19
+price5 = 1.25
+best = min(price1, price2, price3, price4, price5)
+print('best price is', best)
+
+# max()
+worst = max(price1, price2, price3, price4, price5)
+print('worst price is', worst)
+
